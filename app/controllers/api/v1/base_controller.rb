1ce753d2b7e466ecdc7162e9345ea1f8492f1f96
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-02-20 12:34:32 -0500

    Setup cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   before_action :authenticate
+  before_action :set_headers
 
   def authenticate
     @token_str = bearer_token
@@ -18,5 +19,12 @@ class Api::V1::BaseController < ApplicationController
     pattern = /^Bearer /
     header  = request.headers["Authorization"]
     header.gsub(pattern, '') if header && header.match(pattern)
+  end
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
