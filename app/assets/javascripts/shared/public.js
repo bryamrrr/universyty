674b8ba0accc63d427a85675422db68d79e1c50b
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-03-30 21:29:11 -0500

    Add responsive to campus welcome view

--- app/assets/javascripts/shared/public.js
+++ app/assets/javascripts/shared/public.js
@@ -2,4 +2,8 @@ $(document).on("ready", function () {
   $("body").on('click', '#settings', function () {
     $(".aside-menu").toggleClass("open");
   });
+
+  $('#toggle-aside').on('click', function () {
+    $("#aside").toggleClass("show");
+  });
 })
