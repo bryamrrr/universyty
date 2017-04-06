007fa69a8bb79a6950c6830d5cbcd97ff46d2962
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-04-06 14:47:14 -0500

    Remove limit in bonos index

--- app/controllers/api/v1/bonos_controller.rb
+++ app/controllers/api/v1/bonos_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BonosController < Api::V1::BaseController
 
   def index
-    bonos = @current_user.bonos.order(created_at: :desc).limit(15)
+    bonos = @current_user.bonos.order(created_at: :desc)
     render :json => bonos.to_json
   end
 
